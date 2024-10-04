0819ba6f4f53bb4ada85b05ca44a3bd32644fa2f
Najmuzzama Khan <48179146+NzkFalconet@users.noreply.github.com>
2024-10-04 20:37:32 +0530

    Delete customer-form directory

--- customer-form/models/Customer.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const CustomerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String, required: true, unique: true },
-});
-
-export default mongoose.models.Customer || mongoose.model('Customer', CustomerSchema);
